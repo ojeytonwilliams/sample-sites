51bb4ce146ec48573d7addf043a298c742cd2677
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2016-08-29 16:41:51 +0200

    Added two new routes.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var cool = require('cool-ascii-faces');
 var express = require('express');
 var app = express();
 
@@ -11,6 +12,14 @@ app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
   response.render('pages/index');
+});
+
+app.get('/cool', function(request, response) {
+  response.send(cool());
+});
+
+app.get('/simpletext', function(request, response) {
+  response.send("A string of uninteresting text");
 });
 
 app.listen(app.get('port'), function() {
