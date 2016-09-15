24694b60685002ee98536262cc233827766c1126
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2016-09-15 16:20:12 +0200

    Timers can now be added and deleted programatically.

--- public/scripts/timer.js
+++ public/scripts/timer.js
@@ -21,25 +21,32 @@ function Timer(id) {
   }
 
   this.updateDisplay = function (hours, minutes, seconds) {
-    var timerEl = $(".timer#" + id);
-    timerEl.find(".seconds").text(seconds);
-    console.log(timerEl.find(".seconds"));
-    console.log(timerEl);
+    var timerEl = $("#" + id);
+    timerEl.find("input.hours").val(hours)
+    timerEl.find("input.minutes").val(minutes)
+    timerEl.find("input.seconds").val(seconds)
     console.log(`id ${this.id} hours ${hours} minutes ${minutes} seconds ${seconds}`)
   }
 
-  this.startTimer = function (duration) {
+  this.start = function (duration) {
     console.log("duration " + duration);
     console.log(Number(duration));
     if (this.timerId == undefined) {
-      if(Number(duration)){
-        this.timerId = setInterval(this.updateTimer.bind(this), 1000, Date.now() + Number(duration));
+      if (Number(duration)) {
+        this.timerId = setInterval(this.updateTimer.bind(this), 100, Date.now() + Number(duration));
       } else {
         console.log("Malformed time input.");
       }
     } else {
       console.log("Timer already running.");
     } // otherwise we simply ignore it, since we want each Timer to only deal with one interval.
+  }
+
+  this.stop = function(){
+
+    clearInterval(this.timerId);
+    // the reference needs to be null, so that the timer can be restarted.
+    this.timerId = null;
   }
 }
 
